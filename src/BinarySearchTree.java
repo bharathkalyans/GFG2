e01ce8ed8d60eacefe91e4d5fd0a21ec79af9ab5
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-05 20:01:00 +0530

    Working on BST

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,35 @@
+import javax.swing.plaf.metal.MetalButtonUI;
+import java.util.Scanner;
+
+class Node{
+
+    int key;
+    Node left;
+    Node right;
+
+    Node(int key){
+        this.key = key;
+    }
+}
+
+public class BinarySearchTree {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+
+
+    public boolean searchKey(Node head,int key){
+        if (head.key == key)
+            return true;
+        else if (head.key >key){
+            return searchKey(head.left,key);
+        }
+        else if (head.key < key){
+            return searchKey(head.right,key);
+        }
+        return false;
+    }
+}
