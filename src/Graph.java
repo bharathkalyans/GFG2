6d7cf8922bb2e901bdbb0fcbf27d47f37b517063
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-15 10:44:13 +0530

    Added BFS for Graph Traversal

--- src/Graph.java
+++ src/Graph.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Graph {
 
@@ -20,31 +20,50 @@ public class Graph {
         }
     }
 
+
+
+    public static void BFS(ArrayList<ArrayList<Integer>> adj,int v,int s){
+
+        Queue<Integer> q=new LinkedList<>();
+
+        Boolean [] visited = new Boolean[v+1];
+        for(int i = 0; i < v; i++)
+            visited[i] = false;
+
+        visited[s] = true;
+        q.add(s);
+
+        while(!q.isEmpty()){
+            int u = q.poll();
+            System.out.print(u+" ");
+
+            for (int x: adj.get(u)){
+                if (visited [x] == false){
+                    visited[x] = true;
+                    q.add(x);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
 
         int vertices = 5;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-/*
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Vertices :: ");
-        vertices = sc.nextInt();
-*/
-
         for (int i=0;i<vertices;i++)
-            adj.add(new ArrayList<>());
+            adj.add(new ArrayList<Integer>());
 
 
         addEdge(adj,0,1);
         addEdge(adj,0,2);
         addEdge(adj,1,2);
         addEdge(adj,2,3);
+        addEdge(adj,1,3);
         addEdge(adj,3,4);
-        printGraph(adj);
+        addEdge(adj,2,4);
 
 
-
-
+        BFS(adj,vertices,2);
 
 
     }
