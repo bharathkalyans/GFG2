4271e215a3e675d86bfb3ec08fc29ccbf8dc0c45
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-23 19:45:05 +0530

    Added Articulation Point Function

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
 public class Graph {
+    private int V;   // No. of vertices
+
+    // Array  of lists for Adjacency List Representation
+    private LinkedList<Integer> adj[];
+    int time = 0;
+    static final int NIL = -1;
+
+    // Constructor
+    Graph(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
 
     public static void addEdge(ArrayList<ArrayList<Integer>> adj,int u,int v){
         adj.get(u).add(v);
@@ -254,19 +269,19 @@ public class Graph {
             System.out.println(x);
     }
 
-    static final int V=4;
+    static final int Vert=4;
     public static int[] dijkstra(int graph[][], int src) {
 
-        int[] dist=new int[V];int res=0;
+        int[] dist=new int[Vert];int res=0;
         Arrays.fill(dist,Integer.MAX_VALUE);dist[src]=0;
-        boolean[] fin=new boolean[V];
+        boolean[] fin=new boolean[Vert];
 
-        for (int count = 0; count < V-1 ; count++)
+        for (int count = 0; count < Vert-1 ; count++)
         {
             int u = -1;
  
             //Finds the minimum value of all vertices
-            for(int i=0;i<V;i++)
+            for(int i=0;i<Vert;i++)
                 if(!fin[i]&&(u==-1||dist[i]<dist[u]))
                     u=i;
 
@@ -274,7 +289,7 @@ public class Graph {
             fin[u] = true;
 
             //Relaxes the Adjacent vertices of the Vertex Selected .
-            for (int v = 0; v < V; v++)
+            for (int v = 0; v < Vert; v++)
 
                 if (graph[u][v]!=0 && fin[v] == false)
                     dist[v] = Math.min(dist[v],dist[u]+graph[u][v]);//Relax Operation.
@@ -282,13 +297,91 @@ public class Graph {
         return dist;
     }
 
-    public static void TopologicalSort(ArrayList<ArrayList<Integer>> adj,int v){
-        Boolean visited[]= new Boolean[v];
-        Arrays.fill(visited,false);
+    // A recursive function that find articulation points using DFS
+    // u --> The vertex to be visited next
+    // visited[] --> keeps tract of visited vertices
+    // disc[] --> Stores discovery times of visited vertices
+    // parent[] --> Stores parent vertices in DFS tree
+    // ap[] --> Store articulation points
+    void APUtil(int u, boolean visited[], int disc[],
+                int low[], int parent[], boolean ap[])
+    {
 
+        // Count of children in DFS Tree
+        int children = 0;
 
+        // Mark the current node as visited
+        visited[u] = true;
 
+        // Initialize discovery time and low value
+        disc[u] = low[u] = ++time;
 
+        // Go through all vertices adjacent to this
+        Iterator<Integer> i = adj[u].iterator();
+        while (i.hasNext())
+        {
+            int v = i.next();  // v is current adjacent of u
+
+            // If v is not visited yet, then make it a child of u
+            // in DFS tree and recur for it
+            if (!visited[v])
+            {
+                children++;
+                parent[v] = u;
+                APUtil(v, visited, disc, low, parent, ap);
+
+                // Check if the subtree rooted with v has a connection to
+                // one of the ancestors of u
+                low[u]  = Math.min(low[u], low[v]);
+
+                // u is an articulation point in following cases
+
+                // (1) u is root of DFS tree and has two or more chilren.
+                if (parent[u] == NIL && children > 1)
+                    ap[u] = true;
+
+                // (2) If u is not root and low value of one of its child
+                // is more than discovery value of u.
+                if (parent[u] != NIL && low[v] >= disc[u])
+                    ap[u] = true;
+            }
+
+            // Update low value of u for parent function calls.
+            else if (v != parent[u])
+                low[u]  = Math.min(low[u], disc[v]);
+        }
+    }
+
+    // The function to do DFS traversal.
+    // It uses recursive function APUtil()
+    void AP()
+    {
+        // Mark all the vertices as not visited
+        boolean visited[] = new boolean[V];
+        int disc[] = new int[V];
+        int low[] = new int[V];
+        int parent[] = new int[V];
+        boolean ap[] = new boolean[V]; // To store articulation points
+
+        // Initialize parent and visited, and ap(articulation point)
+        // arrays
+        for (int i = 0; i < V; i++)
+        {
+            parent[i] = NIL;
+            visited[i] = false;
+            ap[i] = false;
+        }
+
+        // Call the recursive helper function to find articulation
+        // points in DFS tree rooted with vertex 'i'
+        for (int i = 0; i < V; i++)
+            if (visited[i] == false)
+                APUtil(i, visited, disc, low, parent, ap);
+
+        // Now ap[] contains articulation points, print them
+        for (int i = 0; i < V; i++)
+            if (ap[i] == true)
+                System.out.print(i+" ");
     }
 
 
