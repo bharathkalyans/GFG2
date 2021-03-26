0c30dc3ac11a786f1e8a39e0a50c05b0ea23407b
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-26 11:33:55 +0530

    String not containing "AB"(compare)

--- src/BackTracking.java
+++ src/BackTracking.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class BackTracking {
 
     public static void permutationOfString(String string,String remaining){
@@ -22,6 +20,31 @@ public class BackTracking {
     }
 
 
+    public static void stringNotContainingAString(String str, int l, int r){
+    //GFG Solution
+        if(l==r){
+            if(!str.contains("AB")){
+                System.out.print(str+" ");
+            }
+        }else{
+            for(int i=l;i<=r;i++){
+                str=new String(swap(str, i, l));
+
+                stringNotContainingAString(str,l+1,r);
+
+                str=new String(swap(str, i, l));
+            }
+        }
+    }
+
+    public static char[] swap(String str, int i, int j) {
+        char[] ch = str.toCharArray();
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
+        return ch;
+    }
+
 
     public static void lexicographic(char[] chars, String res) {
         if (res.length() == chars.length) {
@@ -37,9 +60,40 @@ public class BackTracking {
         }
     }
 
+
+    public static void stringNotContainingAString(String string,
+                                                  String result
+                                                  ,String compare){
+
+        if (string.length() == 0){
+            if (!result.contains(compare)) {
+                System.out.print(result+" ");
+            }
+        }
+
+        for (int i=0;i<string.length();i++){
+            String newResult = result + string.charAt(i);
+            String remainingString = string.substring(0,i) +
+                    string.substring(i+1);
+
+            stringNotContainingAString(remainingString,newResult,compare);
+
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
+        System.out.println("Normal Permutation");
         permute("ABC");
         System.out.println();
+
+        System.out.println("GFG Solution");
+        stringNotContainingAString("ABC",0,2);
+
+        System.out.println();
+        System.out.println("My Own Solution");
+        stringNotContainingAString("ABC","","AB");
     }
 }
