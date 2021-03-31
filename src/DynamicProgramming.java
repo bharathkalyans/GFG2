249d5abafeb922b55473280d009667813f0e647e
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-31 13:01:28 +0530

    Printing LCS Function Added

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DynamicProgramming {
 
@@ -98,9 +100,46 @@ public class DynamicProgramming {
 
     }
 
+    public static void printMemoTable(){
+        for (int i=0;i< memo.length;i++){
+            for (int j=0;j< memo[i].length;j++)
+                System.out.print(memo[i][j]+" ");
+            System.out.println();
+        }
+    }
+
+    public static Set<String> printLCS(String X, String Y, int m, int n) {
+        Set<String> s = new HashSet<>();
+
+        if (m == 0 || n == 0) {
+            s.add("");
+            return s;
+        }
+
+        if (X.charAt(m - 1) == Y.charAt(n - 1)) {
+
+            Set<String> tmp = printLCS(X, Y, m - 1, n - 1);
+
+
+            for (String str : tmp)
+                s.add(str + X.charAt(m - 1));
+        }
+
+        else {
+            if (memo[m - 1][n] >= memo[m][n - 1])
+                s = printLCS(X, Y, m - 1, n);
+
+            if (memo[m][n - 1] >= memo[m - 1][n]) {
+                Set<String> tmp = printLCS(X, Y, m, n - 1);
+
+                s.addAll(tmp);
+            }
+        }
+        return s;
+    }
     public static void main(String[] args) {
 
-        String s1="AXYZ", s2="BAZ";
+        String s1="ABC", s2= "BCD";
 
         int n = s1.length();
         int m = s2.length();
@@ -112,6 +151,10 @@ public class DynamicProgramming {
 
 
         System.out.println(lcs(s1,s2,n,m));
+        printMemoTable();
+        Set<String > s = printLCS(s1,s2,n,m);
 
+        for (String ss: s)
+            System.out.println(ss);
     }
 }
