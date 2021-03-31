2913bd9f117db55136f95bc5121a12dbac22e648
Bharath Kalyan S <sbharath35@gmail.com>
2021-03-31 11:33:29 +0530

    LongestCommonSubSequence DP Solution

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -78,11 +78,40 @@ public class DynamicProgramming {
                     LongestCommonSubSequence(s1,s2,m,n-1));
     }
 
+    static int[][] memo;
+
+    static int lcs(String s1, String s2, int n, int m) {
+        if(memo[n][m]!=-1)
+            return memo[n][m];
+
+        if(n==0 || m==0)
+            memo[n][m]=0;
+
+        else {
+            if(s1.charAt(n-1)==s2.charAt(m-1))
+                memo[n][m] = 1 + lcs(s1,s2,n-1,m-1);
+            else
+                memo[n][m] = Math.max(lcs(s1,s2,n-1,m),lcs(s1,s2,n,m-1));
+        }
+
+        return memo[n][m];
+
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("Longest SubSequence is "+
-                LongestCommonSubSequence("Bharath","Bharath", 7,7));
+        String s1="AXYZ", s2="BAZ";
+
+        int n = s1.length();
+        int m = s2.length();
+
+        memo= new int[n+1][m+1];
+
+        for(int[] i: memo)
+            Arrays.fill(i,-1);
+
+
+        System.out.println(lcs(s1,s2,n,m));
 
     }
 }
