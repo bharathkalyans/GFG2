d0006d676758d91cfaca51c3fea12924e8f40759
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-03 11:59:23 +0530

    LongestIncreasingSubSequence Binary Search Solution

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -227,7 +227,6 @@ public class DynamicProgramming {
 
     public static int LongestIncreasingSubSequence(int[] arr,int n){
 
-
         int [] lis = new int[n];
 
         lis[0] = 1;
@@ -243,17 +242,48 @@ public class DynamicProgramming {
             System.out.print(lis[i]+" ");
             result = Math.max(result,lis[i]);
         }
-
+        System.out.println();
         return result;
 
     }
 
+    //Efficient Way of Solving the Above Function in O(nLog n) time.
+    public static int LongestIncreasingSubSequenceEff(int[] arr,int n){
+
+        int tail[] =new int[n];
+
+        tail[0] = arr[0];
+        int len =1;
+
+        for (int i=1;i<n;i++){
+            if (arr[i]> tail[len -1]){
+                tail[len] = arr[i];
+                len++;
+            }else {
+                int c =ceilIndex(tail,0,len -1,arr[i]);
+                tail[c] =arr[i];
+            }
+        }
+        return len;
+    }
+
+    public static int ceilIndex(int[] tail,int l,int r,int x){
+        while (r>l){
+            int mid = l + (r-l)/2 ;
+            if (tail[mid] >= x)
+                r= mid;
+            else
+                l= mid+1;
+        }
+        return r;
+    }
 
     public static void main(String[] args) {
 
         int [] a = {3,4,2,8,10};
 
         System.out.println(LongestIncreasingSubSequence(a,a.length));
+        System.out.println(LongestIncreasingSubSequenceEff(a,a.length));
 
     }
 }
