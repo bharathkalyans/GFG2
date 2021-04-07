a24f18c3d3e0c06db1bbdd3b37d5147bd1fea730
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-07 11:52:17 +0530

    Optimal Strategy Game 😰

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -381,6 +381,28 @@ public class DynamicProgramming {
 
         return dp[n][W];
     }
+
+    public static int optimalStartegyGame(int[] arr, int n) {
+        int dp[][] = new int[n][n];
+
+        for(int i=0;i<n-1;i++)
+        {
+            dp[i][i+1]= Math.max(arr[i],arr[i+1]);
+        }
+
+        for(int gap =3; gap<n; gap = gap + 2)
+        {
+            for(int i=0; i+gap<n; i++)
+            {
+                int j = gap + i;
+
+                dp[i][j] = Math.max((arr[i] + Math.min(dp[i+1][j], dp[i+1][j-1])),
+                        (arr[i] + Math.min(dp[i+1][j-1], dp[i][j-2])));
+            }
+        }
+
+        return dp[0][n-1];
+    }
     public static void main(String[] args) {
 
 
