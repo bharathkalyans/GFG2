879e3c81133de21b605e4086e8939043e314ca09
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-12 11:49:31 +0530

    Count Max Subsets with Given Sum !

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -447,6 +447,15 @@ public class DynamicProgramming {
 
         return dp[n];
     }
+
+    public static int countMaxSubSet(int [] arr,int n,int sum){
+        if (n == 0)
+            return sum == 0 ? 1:0;
+        return countMaxSubSet(arr,n-1,sum) +
+                countMaxSubSet(arr,n-1,sum - arr[n-1]);
+
+    }
+
     public static void main(String[] args) {
 
 
